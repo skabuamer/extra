2f7ed2aa2dee617282a2a6ee7c3be3992fd1649a
skabuamer <shaikhabuamer@gmail.com>
2022-06-11 15:12:19 +0600

    two-more-section-added

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// const btn = document.getElementById('slider-icon-left');
+
+// const change = () => {
+//     // btn.classList.remove('fa-arrow-left')
+//     // btn.classList.add('fa-arrow-left-long')
+
+//     btn.remove();
+
+//     // const inSpan = btn
+
+//     const newBtn = document.createElement('i') 
+
+//     newBtn.innerHTML = `<i class="fa-solid fa-arrow-left-long"></i>`
+
+
+
+//     console.log(btn)
+//     console.log(newBtn)
+// }
